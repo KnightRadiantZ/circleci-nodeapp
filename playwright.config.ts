c1da2f2867c5f084903ed512694a61ca44ae3537
KnightRadiantZ <joseph.evasco@gmail.com>
2023-03-15 16:51:28 -0700

    Add xml reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,10 @@
 import { PlaywrightTestConfig } from '@playwright/test'
+import { defineConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   testDir: './specs/playwright',
   maxFailures: 2,
+  reporter: [['junit', { outputFile: 'out/report/results.xml' }]],
 }
 
 export default config
